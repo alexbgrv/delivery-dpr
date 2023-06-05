e83339e20c3ea4066dee82fb8892eaa1a1086e9f
DreamMavpa <dreammavpa@gmail.com>
2023-06-05 08:07:47 +0300

    добавил пнг и css и идите нахуй

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import wb from './img/wb.png'; 
+import ali from './img/ali.png'; 
+import ozon from './img/ozon.png'; 
+import yandex from './img/yandex.png'; 
 
 function App() {
   return (
-    <h1>Че то сделал че то глянул</h1>
+    <div className="wrapper-conteiner">
+      <h1>Выберите маркетплейс</h1>
+      <div className="conteiner-Widget">
+        <button><img src={wb} alt="image wb" /></button>
+        <button><img src={ali} alt="image ali" /></button>
+        <button><img src={ozon} alt="image ozon" /></button>
+        <button><img src={yandex} alt="image yandex" /></button>
+      </div>
+    </div>
   );
 }
 
